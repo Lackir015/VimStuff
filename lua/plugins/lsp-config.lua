b7b5b05fadff6bea1ebf8f7c4f7727c0bb70772b
Lackir015 <ryanlack@buffalo.edu>
2024-08-20 22:45:38 -0400

    Added underlined errors and warnings instead of it crowding the window. Also added a keybind to show the error message/warning in a popup window

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,47 +1,58 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.bashls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.gopls.setup({
-        capabilites = capabilities,
-      })
-      lspconfig.jdtls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.bashls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.gopls.setup({
+				capabilites = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.diagnostic.config({
+				virtual_text = false,
+				signs = false,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+			--Key mappings for the LSP's
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+			--Diagnostics on just hovering wasn't working so I just created a keybind to open a window with diagnostics
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
+		end,
+	},
 }
